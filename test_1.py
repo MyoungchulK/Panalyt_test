0ddaf1c2c2eb4f6b87a37dc8d36e30e0d057f642
MyoungchulK <kmc8907@gmail.com>
2024-04-26 15:28:22 +0900

    first

---
+++ test_1.py
@@ -0,0 +1,28 @@
+import numpy as np
+import click
+
+@click.command()
+@click.option('-tg', '--target', default = '1,51')
+@click.option('-t1', '--thres1', default = 3, type = int)
+@click.option('-r1', '--replace1', default = 'Pana', type = str)
+@click.option('-t2', '--thres2', default = 5, type = int)
+@click.option('-r2', '--replace2', default = 'Lyt', type = str)
+def main(target, thres1, replace1, thres2, replace2):
+
+    ## array creation
+    tg_param = np.asarray(target.split(',')).astype(int)
+    tg_arr = np.arange(tg_param[0], tg_param[1])
+    tg_obj = tg_arr.astype(object)
+
+    ## replace
+    t1_bool = tg_arr % thres1 == 0
+    t2_bool = tg_arr % thres2 == 0
+    tg_obj[t1_bool] = replace1
+    tg_obj[t2_bool] = replace2
+    tg_obj[np.logical_and(t1_bool, t2_bool)] = replace1 + replace2
+    
+    print(tg_obj)
+
+if __name__ == "__main__":
+    main()
+
